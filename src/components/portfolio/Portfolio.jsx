636c8ac61b1b440c43d9e0120af7c10f34b257ed
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-17 20:44:43 +0300

    portfolio completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,30 @@ import "../portfolio/portfolio.scss";
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Tour Places</h3>
+                </div>
+            </div>
         </div>
     )
 }
