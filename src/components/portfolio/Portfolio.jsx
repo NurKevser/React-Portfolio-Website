0789342d1dc1b33fe59566d0a18e1b625ae18e04
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-18 18:26:43 +0300

    portfolioList completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,31 +1,60 @@
 import "../portfolio/portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useState ,useEffect } from "react";
+import { featuredPortfolio, webPortfolio, } from "../../data";
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
+    const list = [
+        {
+          id: "featured",
+          title: "Featured",
+        },
+        {
+          id: "web",
+          title: "Web App",
+        },
+    
+      ];
+
+      useEffect(() => {
+
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+                default:
+                    setData(featuredPortfolio);
+        }
+
+      }, [selected]);
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Web App</li>
-                <li>Design</li>
+                {list.map((item) => (
+                    <PortfolioList 
+                    title={item.title} 
+                    active={selected === item.id} 
+                    setSelected={setSelected}
+                    id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
+                {data.map(d => (
                 <div className="item">
-                    <img src="" alt="" />
-                    <h3>Tour Places</h3>
+                    <img src={d.img} alt="" />
+                    <h3>{d.title}</h3>
                 </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Tour Places</h3>
-                </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Tour Places</h3>
-                </div>
-                <div className="item">
-                    <img src="" alt="" />
-                    <h3>Tour Places</h3>
-                </div>
+
+                ))}
+                
             </div>
         </div>
     )
