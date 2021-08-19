e3b9c5471a1c8c5ccd61b06c4446a2ebbd23d757
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-19 15:28:16 +0300

    created slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
 import "../works/works.scss";
 
 export default function Works() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const data = [
+        {
+          id: "1",
+          icon: "./assets/mobile.png",
+          title: "Web Design",
+          desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+          img:
+            "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+        },
+        {
+          id: "2",
+          icon: "./assets/globe.png",
+          title: "Mobile Application",
+          desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          img:
+            "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+        },
+        {
+          id: "3",
+          icon: "./assets/writing.png",
+          title: "Branding",
+          desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          img:
+            "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+        },
+      ];
+      
+      const handleClick = (way) =>{
+          way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) : setCurrentSlide(currentSlide < data.length -1 ? currentSlide+1 : 0);
+      };
     return (
         <div className="works" id="works">
-            <div className="slider">
-                <div className="container">
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+                {data.map(d => (
+
+                    <div className="container">
                     <div className="item">
                         <div className="left">
                             <div className="leftContainer">
                                 <div className="imgContainer">
-                                    <img src="assets/mobile.png" alt="" />
+                                    <img src={d.icon} alt="" />
                                 </div>
-                                <h2>Title</h2>
+                                <h2>{d.title}</h2>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, aut error vel corporis quidem.
+                                   {d.desc}
                                 </p>
                                 <span>Projects</span>
                             </div>
@@ -22,10 +60,11 @@ export default function Works() {
                             <img src="https://cdn.pixabay.com/photo/2021/08/08/15/45/mountains-6531264_1280.jpg" alt="" />
                         </div>
                     </div>
-                </div>
+                    
+                </div>))}
             </div>
-            <img src="assets/arrow.png" className="arrow left" alt="" />
-            <img src="assets/arrow.png" className="arrow right" alt="" />
+            <img src="assets/arrow.png" className="arrow left" alt=""  onClick={()=> handleClick("left")} />
+            <img src="assets/arrow.png" className="arrow right" alt="" onClick={()=>handleClick()} />
         </div>
     )
 }
